3027af3ee3310bcc2e78c264f555b3c8e2a0c42c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 10:45:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(shout)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif (shout == shout.upcase)
     return "NO, NOT SINCE 1938!"
-  else == (shout!= shout.downcase)
+  else (shout== shout.downcase)
     return "HUH?! SPEAK UP, SONNY!"
 
   end
