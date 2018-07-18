cf71584cc68a9910f5c91289e8784acd59b21474
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 10:32:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,14 +13,13 @@
 #
 
 def speak_to_grandma(shout)
-  if shout.downcase
+  if shout != downcase
     return "HUH?! SPEAK UP, SONNY!"
-
   elsif shout == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif shout =! phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else shout.downcase
+  else != shout.downcase
     return "HUH?! SPEAK UP, SONNY!"
 
   end
